95ce7466ce485b801aaac28fe3ee979a056cf9c8
Enzo949494 <9812@holbertonstudents.com>
2025-04-01 13:13:23 +0200

    task6, try to fix with add string in result reason

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
+  ];
+
+  const results = await Promise.allSettled(promises);
+  return results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  }));
 }
