88dea6564812f29ecb43a373adae71658cb2957e
Enzo949494 <9812@holbertonstudents.com>
2025-04-01 14:30:49 +0200

    task9, fix with $ error

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,11 +1,14 @@
 export default function guardrail(mathFunction) {
   const queue = [];
+
   try {
-    queue.push(mathFunction());
+    const result = mathFunction();
+    queue.push(result);
   } catch (error) {
-    queue.push(String(error));
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
+
   return queue;
 }
