399a315beae4c59ce52d8e9a7f8773118adf8a6f
Enzo949494 <9812@holbertonstudents.com>
2025-04-23 12:26:47 +0200

    task1, fix pystyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -29,7 +29,8 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     for delay in delays:
         # Insert delay in the correct position to maintain ascending order
         position = 0
-        while position < len(sorted_delays) and delay > sorted_delays[position]:
+        while (position < len(sorted_delays) and 
+               delay > sorted_delays[position]):
             position += 1
         sorted_delays.insert(position, delay)
     
