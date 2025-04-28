af19bf9c84042de4e335d971b103c2570e8a930b
Enzo949494 <9812@holbertonstudents.com>
2025-04-28 13:10:20 +0200

    delete blankline between doctype and for

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -10,7 +10,6 @@ async def async_generator() -> AsyncGenerator[float, None]:
     coroutine that loops 10 times, waiting 1 second for each
     time and yields random number between 0 and 10.
     """
-
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
