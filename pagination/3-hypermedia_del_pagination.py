a1493765cd59b8f4b3b45ddb3224e7c74e01939e
Enzo949494 <9812@holbertonstudents.com>
2025-04-29 12:39:49 +0200

    try to fix task3, modify page_size in return

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -81,6 +81,6 @@ class Server:
         return {
             'index': index,
             'next_index': next_index,
-            'page_size': page_size,
+            'page_size': len(data),
             'data': data
         }
