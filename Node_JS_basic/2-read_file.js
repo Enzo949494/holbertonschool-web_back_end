fa6dd55f3ae15a2dfbeee6720ab0e92b73c39ed6
Enzo949494 <9812@holbertonstudents.com>
2025-05-19 13:22:48 +0200

    task2, ttf again, more simply code

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -9,30 +9,26 @@ function countStudents(path) {
   }
 
   const lines = data.split('\n');
-  // Remove empty lines and header
   const students = lines.filter((line) => line && line !== lines[0]);
-  
   console.log(`Number of students: ${students.length}`);
-  
+
   const fields = {};
   const columns = lines[0].split(',');
   const firstNamePos = columns.indexOf('firstname');
   const fieldPos = columns.indexOf('field');
-  
-  students.forEach((student) => {
+
+  for (const student of students) {
     const studentData = student.split(',');
     const field = studentData[fieldPos];
     const firstName = studentData[firstNamePos];
-    
-    if (!fields[field]) fields[field] = { count: 0, names: [] };
-    fields[field].count += 1;
-    fields[field].names.push(firstName);
-  });
-  
+
+    if (!fields[field]) fields[field] = [];
+    fields[field].push(firstName);
+  }
+
   for (const field in fields) {
     if (Object.prototype.hasOwnProperty.call(fields, field)) {
-      const { count, names } = fields[field];
-      console.log(`Number of students in ${field}: ${count}. List: ${names.join(', ')}`);
+      console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
     }
   }
 }
