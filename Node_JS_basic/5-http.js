c9bc7df4c5e2f7fcb09d27d8fd33e7662b2d447a
Enzo949494 <9812@holbertonstudents.com>
2025-05-19 14:18:30 +0200

    task5, ttf with async await method

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,60 +1,52 @@
 const http = require('http');
 const fs = require('fs').promises;
 
-const countStudents = (path) => {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8')
-      .then((data) => {
-        const lines = data.split('\n');
-        const students = lines.filter((line) => line && line !== lines[0]);
-        let output = `Number of students: ${students.length}\n`;
+const countStudents = async (path) => {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n');
+    const students = lines.filter((line) => line && line !== lines[0]);
+    let output = `Number of students: ${students.length}`;
 
-        const fields = {};
-        const columns = lines[0].split(',');
-        const firstNamePos = columns.indexOf('firstname');
-        const fieldPos = columns.indexOf('field');
+    const fields = {};
+    const columns = lines[0].split(',');
+    const firstNamePos = columns.indexOf('firstname');
+    const fieldPos = columns.indexOf('field');
 
-        for (const student of students) {
-          const studentData = student.split(',');
-          const field = studentData[fieldPos];
-          const firstName = studentData[firstNamePos];
+    for (const student of students) {
+      const studentData = student.split(',');
+      const field = studentData[fieldPos];
+      const firstName = studentData[firstNamePos];
 
-          if (!fields[field]) fields[field] = [];
-          fields[field].push(firstName);
-        }
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstName);
+    }
 
-        for (const field in fields) {
-          if (Object.prototype.hasOwnProperty.call(fields, field)) {
-            output += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-          }
-        }
-        
-        // Remove the last newline
-        output = output.slice(0, -1);
-        resolve(output);
-      })
-      .catch(() => {
-        reject(new Error('Cannot load the database'));
-      });
-  });
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        output += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
+      }
+    }
+    
+    return output;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 };
 
-const app = http.createServer((req, res) => {
+const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   
   if (req.url === '/') {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    
-    countStudents(process.argv[2])
-      .then((result) => {
-        res.end(result);
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
+    try {
+      const studentsInfo = await countStudents(process.argv[2]);
+      res.end(`This is the list of our students\n${studentsInfo}`);
+    } catch (error) {
+      res.end(`This is the list of our students\n${error.message}`);
+    }
   } else {
     res.statusCode = 404;
     res.end('Not found');
