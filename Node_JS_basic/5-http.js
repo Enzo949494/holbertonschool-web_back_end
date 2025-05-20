f039582ec7971c12d62eed0910d2c33763f76fb7
Enzo949494 <9812@holbertonstudents.com>
2025-05-20 10:25:56 +0200

    task5, ttf with simply code

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -8,34 +8,30 @@ function countStudents(path) {
         reject(new Error('Cannot load the database'));
         return;
       }
-      
       const lines = data.split('\n').filter(line => line.trim());
       if (lines.length <= 1) {
         resolve('Number of students: 0');
         return;
       }
-
-      const columns = lines[0].split(',');
-      const fieldIndex = columns.indexOf('field');
-      const firstnameIndex = columns.indexOf('firstname');
-      
+      const headers = lines[0].split(',');
+      const fieldIndex = headers.indexOf('field');
+      const firstnameIndex = headers.indexOf('firstname');
       const students = lines.slice(1);
       const fields = {};
-
       students.forEach((student) => {
         const values = student.split(',');
         const field = values[fieldIndex];
         const firstname = values[firstnameIndex];
-        
-        fields[field] = fields[field] || [];
-        fields[field].push(firstname);
+        if (field && firstname) {
+          if (!fields[field]) fields[field] = [];
+          fields[field].push(firstname);
+        }
       });
-      
       let report = `Number of students: ${students.length}`;
-      Object.keys(fields).sort().forEach((field) => {
+      // Respecte l'ordre d'apparition
+      Object.keys(fields).forEach((field) => {
         report += `\nNumber of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`;
       });
-      
       resolve(report);
     });
   });
@@ -43,27 +39,20 @@ function countStudents(path) {
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
-  
   if (req.url === '/') {
-    res.statusCode = 200;
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     const databaseFilename = process.argv[2];
-    
     if (!databaseFilename) {
-      res.statusCode = 500;
       res.end('Cannot load the database');
       return;
     }
-
-    res.statusCode = 200;
-    res.write('This is the list of our students\n');
-    
     countStudents(databaseFilename)
-      .then(report => res.end(report))
-      .catch(() => {
-        res.statusCode = 500;
-        res.end('Cannot load the database');
+      .then((report) => {
+        res.end(`This is the list of our students\n${report}`);
+      })
+      .catch((error) => {
+        res.end(error.message);
       });
   } else {
     res.statusCode = 404;
